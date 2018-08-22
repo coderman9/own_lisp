f49c094389b9a22e8b5c7729eb84e2bb3318499e
benjamin <banker@cnm.edu>
2018-08-22 13:41:30 -0600

    windows supp

--- prompt.c
+++ prompt.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+//if we are running on windows. I believe this works for x64 too
+#ifdef _WIN32
+#include <string.h>
 //buffer for user input of size 2048 (bytes, since each char is a byte)
 //note: a static global variable means lexical scope is confined to this "compilation unit" (can probs be understood as file).
 //static inside of a function means that it keeps its value between invocations
-static char input[2048];
+static char buffer[2048];
+
+char* readline(char* prompt)
+{
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	//malloc one extra byte since a string ends with \0
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	//terminate string
+	cpy[strlen(cpy)-1]='\0';
+	return cpy;
+}
+//windows doesn't need the add_history fn
+void add_history(char* unused){}
+
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 //argc is argument count, argv is argument vector
 //char** is a list of strings (approximately)
@@ -14,10 +37,11 @@ int main(int argc, char** argv)
 	puts("Press Ctrl+C to exit\n");
 	while(1)
 	{
-		fputs("lisp> ", stdout);
-		//reads stdin of max size 2048 to new var input
-		fgets(input, 2048, stdin);
-		printf("echo: %s", input);
+		//note doesn't read in newline
+		char* input = readline("lispy> ");
+		add_history(input);
+		printf("echo: %s\n", input);
+		free(input);
 	}
 	return 0;
 }
